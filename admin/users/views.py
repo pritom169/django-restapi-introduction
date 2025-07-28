07aee682545cbbef1eff5f24e7e4c012da0459f1
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-28 15:13:57 +0200

    [ADDITION] A POST api has been created

--- admin/users/views.py
+++ admin/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['POST'])
 def hello(request):
     return Response('hello')
